e9a31c407c07815c763ac0721e95f6bcf5fee09c
mo36924 <mo36924@users.noreply.github.com>
2021-01-08 18:33:00 +0900

    🐛

--- src/access.ts
+++ src/access.ts
@@ -2,7 +2,7 @@ import { access as _access } from "fs/promises";
 
 export async function access(path: string) {
   try {
-    await access(path);
+    await _access(path);
     return true;
   } catch {
     return false;
